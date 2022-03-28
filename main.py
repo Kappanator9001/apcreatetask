87abe80d414f2053786ca12a4d7c8494e9192e98
BuildTools <unconfigured@null.spigotmc.org>
2022-03-27 17:06:41 -0700

    3/27

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import pygame
 from buttons import *
 from utility import *
 pygame.init()
-winSize = 600
+winSize = 800
 w = pygame.display.set_mode([winSize, winSize])
 invertColorsDefault = True
 gaming = True
 
 button = textButton(0,0,100,100)
 button2 = ellipseTextButton(200,100,30,50, 'amogus', colors['cyan'])
+buttons = [button, button2]
 while gaming: 
   for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gaming = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
           x,y = pygame.mouse.get_pos()
+          for button in buttons:
+            button.tryClicked(x,y)
   w.fill(colors['white'])
-  button.draw()
-  button2.draw()
+  for button in buttons:
+    button.draw()
   pygame.display.flip()
