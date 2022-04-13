d8b6b2fb28921ae0364a1bf6cd549fd49103cff8
BuildTools <unconfigured@null.spigotmc.org>
2022-04-12 23:36:42 -0700

    finished color invert + started on file i/o + started on scrollbar

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ pygame.init()
 winSize = 800
 w = pygame.display.set_mode([winSize, winSize])
 invertColorsDefault = True
+if invertColorsDefault:
+  invert(colors)
+fileName = 'cache.txt'
+file = open(fileName, 'r')
 gaming = True
 
 keyboard = Keyboard(0,500,winSize, 300)
@@ -15,9 +19,13 @@ while gaming:
         elif event.type == pygame.KEYDOWN:
           if event.key == pygame.K_d:
             invert(colors)
+            for row in keyboard.keys:
+              for button in row:
+                button.fontcolor = colors['black']
+                button.buttoncolor = colors['white']
         elif event.type == pygame.MOUSEBUTTONDOWN:
           x,y = pygame.mouse.get_pos()
-          
+          #estimate row by y position, then use list slicing with countKeys to figure out what key is being pressed
   w.fill(colors['white'])
   keyboard.draw()
   pygame.display.flip()
